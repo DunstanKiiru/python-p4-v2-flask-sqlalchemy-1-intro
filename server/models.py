3d58972d4c4f1322f5a94c6c531f42ce0fe16ca5
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-13 20:21:24 +0300

    updates user model

--- server/models.py
+++ server/models.py
@@ -21,4 +21,5 @@ class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column
+    username = db.Column(db.String(80), unique=True,nullable=False, index=True)
+    email= db.Column(db.String(120))
